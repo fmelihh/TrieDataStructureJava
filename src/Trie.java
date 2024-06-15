fb958a4b849b849e6bef268d6e565b56eedd1f99
fmelihh <furkanmelihercan.98@gmail.com>
2024-06-15 14:29:49 +0300

    feat: insert has completed.

--- src/Trie.java
+++ src/Trie.java
@@ -1,2 +1,41 @@
+import java.util.HashMap;
+
+/*
+* Each trie has an empty root node, with links (or references) to other nodes,
+* one for each possible alphabetic value.
+*
+*The size of trie is directly correlated to the size of all the possible values that
+* the trie could represent.
+*
+*
+* */
 public class Trie {
+    private class TrieNode {
+        private HashMap<Character, TrieNode> children;
+        private boolean endOfKey;
+
+        public TrieNode() {
+            children = new HashMap<>();
+            endOfKey = false;
+        }
+    }
+
+    private TrieNode root;
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String key) {
+        TrieNode current = root;
+        for (int i = 0; i < key.length(); i++) {
+            char c = key.charAt(i);
+            TrieNode node = current.children.get(c);
+            if (node == null) {
+                node = new TrieNode();
+                current.children.put(c, node);
+            }
+            current = node;
+        }
+        current.endOfKey = true;
+    }
 }
