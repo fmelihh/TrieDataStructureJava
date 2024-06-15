e5d1133da71408e06738c194e5f773bebee0c9b3
fmelihh <furkanmelihercan.98@gmail.com>
2024-06-15 14:45:55 +0300

    feat: tests

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,12 @@
 public class Main {
     public static void main(String[] args) {
        Trie trie = new Trie();
+       trie.insert("cat");
+       trie.insert("car");
+       trie.insert("cart");
+
+       System.out.println(trie.contains("cat"));
+       System.out.println(trie.contains("dog"));
+       System.out.println(trie.startsWith("ca"));
     }
 }
